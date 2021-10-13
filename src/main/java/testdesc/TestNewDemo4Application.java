c9ff723e3615d56ddaea201ccccf47a7414e4bd6
shaikhaamer <89386754+shaikhaamer@users.noreply.github.com>
2021-10-13 20:50:26 +0530

    Initial Commit

---
+++ src/main/java/testdesc/TestNewDemo4Application.java
@@ -0,0 +1,13 @@
+package testdesc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestNewDemo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestNewDemo4Application.class, args);
+	}
+
+}
