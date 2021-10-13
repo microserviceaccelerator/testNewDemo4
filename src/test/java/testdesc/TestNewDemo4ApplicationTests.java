6de24d3e2dfbaa5dc4e68bbf2686f1b79cdf6856
shaikhaamer <89386754+shaikhaamer@users.noreply.github.com>
2021-10-13 20:50:26 +0530

    Initial Commit

---
+++ src/test/java/testdesc/TestNewDemo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package testdesc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestNewDemo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
